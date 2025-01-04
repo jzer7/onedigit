5507856610888b656f87518a6cbaffaf7bc8abb9
Juan Rubio <jcrubio@gmail.com>
2025-01-04 14:29:31 -0600

    json output
    Signed-off-by: Juan Rubio <jcrubio@gmail.com>

--- calculate.py
+++ calculate.py
@@ -2,6 +2,8 @@
 """A CLI to calculate number combinations with a single digit."""
 
 import src.onedigit as onedigit
+from json import JSONEncoder
+
 
 def calculate(
     digit: int = 9,
@@ -9,18 +11,26 @@ def calculate(
     *,
     full: bool = False,
     steps: int = 10,
+    format: str = "text",
 ):
     """
     Command line interface for the calculate function.
     """
 
     combos = onedigit.simple.calculate(digit, upper, steps=steps)
-    for c in combos:
-        if full:
-            print(f"{c.value:>4} = {c.expr:<70}   [{c.cost:>3}]")
-        else:
-            print(f"{c.value:>4} = {c.expr_simple:<15}   [{c.cost:>3}]")
-
+    if format == "text":
+        for c in combos:
+            if full:
+                print(f"{c.value:>4} = {c.expr:<70}   [{c.cost:>3}]")
+            else:
+                print(f"{c.value:>4} = {c.expr_simple:<15}   [{c.cost:>3}]")
+    elif format == "json":
+        cc = []
+        for c in combos:
+            cc.append(asdict(c))
+        jsenc = JSONEncoder()
+        jstxt = jsenc.encode(cc)
+        print(jstxt)
 
 if __name__ == '__main__':
 
