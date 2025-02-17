9558edcf57ca811e1b5afd869e041737a126169a
Juan Rubio <jcrubio@gmail.com>
2025-02-17 15:12:25 -0600

    simplify tests, before we add more
    Signed-off-by: Juan Rubio <jcrubio@gmail.com>

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,14 +1,16 @@
-from hypothesis import given, strategies as hst
 import unittest
+
+from hypothesis import given, strategies as hst
 
 import onedigit
 
 # class Model:
 #     digit: int
-#     upper_value: int
-#     state: List[Combo]
+#     max_value: int
+#     max_cost: int
+#     state: Dict[int, Combo]
 #     logger: logging.Logger
-#     def __init__(self, digit: int, upper_value: int = 100, empty: bool = False): ...
+#     def __init__(self, digit: int, max_value: int, max_cost: int, *, shallow: bool = False): ...
 
 
 class Test_Model(unittest.TestCase):
@@ -31,24 +33,24 @@ class Test_Model(unittest.TestCase):
         assert model.state[digit].value == digit
 
     @given(digit=hst.integers(min_value=1, max_value=9))
-    def test_model_creation_good(self, digit):
-
+    def test_model_creation_good(self, digit: int):
         # Good digit
-        model = onedigit.Model(digit=digit, max_value=99, max_cost=4)
-        self.check_model(model, digit)
+        model1 = onedigit.Model(digit=digit, max_value=99, max_cost=4)
+        self.check_model(model1, digit)
 
     @given(digit=hst.integers(min_value=10))
-    def test_model_creation_bad(self, digit):
+    def test_model_creation_bad(self, digit: int):
         # Bad digit
         with self.assertRaises(expected_exception=ValueError):
-            model = onedigit.Model(digit=digit, max_value=99, max_cost=4)
+            model1 = onedigit.Model(digit=digit, max_value=99, max_cost=4)
 
     @given(digit=hst.integers(min_value=1, max_value=9))
-    def test_model_copy(self, digit):
-        # Create a copy and delete the original
+    def test_model_copy_basic(self, digit: int):
+        # Create a model with some combinations
         model1 = onedigit.Model(digit=digit, max_value=99, max_cost=4)
+        # Get a copy and delete the original
         model2 = model1.copy()
         del model1
 
-        # Verify integrity of the copy
+        # Verify integrity of the copy, after we have deleted the original
         self.check_model(model2, digit)
